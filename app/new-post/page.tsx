1650e25347074b789c24f27a20cc595950ca6b81
Arsenii Kondratenko <arsenij.kondratenko@gmail.com>
2025-07-30 13:01:42 +0200

    1-commit

--- app/new-post/page.tsx
+++ app/new-post/page.tsx
@@ -1,3 +1,6 @@
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
+
 export default function NewPostPage() {
   async function createPost(formData: FormData) {
     "use server";
@@ -6,33 +9,28 @@ export default function NewPostPage() {
     const content = formData.get("content");
 
     console.log(title, image, content);
+
+    let errors = [];
+
+    if (!title) {
+      errors.push("Title is required");
+    } else if (!content) {
+      errors.push("Content is required.");
+    } else if (!image) {
+      errors.push("Image is required.");
+    }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
+
+    await storePost({
+      imageUrl: "",
+      title,
+      content,
+      userId: 1,
+    });
+
+    redirect("/feed");
   }
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows={5} />
-        </p>
-        <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
-        </p>
-      </form>
-    </>
-  );
 }
---
+++ components/post-form.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import FormSubmit from "@/components/form-submit";
+import { useActionState } from "react";
+
+export default function PostForm() {
+  const [state, formAction] = useActionState(createPost, {});
+  return (
+    <>
+      <h1>Create a new post</h1>
+      <form action={createPost}>
+        <p className="form-control">
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" name="title" required />
+        </p>
+        <p className="form-control">
+          <label htmlFor="image">Image URL</label>
+          <input
+            type="file"
+            accept="image/png, image/jpeg"
+            id="image"
+            name="image"
+            required
+          />
+        </p>
+        <p className="form-control">
+          <label htmlFor="content">Content</label>
+          <textarea id="content" name="content" rows={5} required />
+        </p>
+        <FormSubmit />
+      </form>
+    </>
+  );
+}
